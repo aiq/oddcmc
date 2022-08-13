2d8152b77827c53d5352703b605a5eaa05f063c4
Alexander Schulz <aiqpost@yahoo.com>
2022-08-13 14:40:54 +0200

    added some ids

--- src/oddcmc/cmcdecl.c
+++ src/oddcmc/cmcdecl.c
@@ -11,11 +11,10 @@
 #define mult_ true
 #define single_ false
 
-
 #define oCMC_DECL_LIST_                                                \
-   XMAP_C_( O_Cmc, "CMC", 0x11CAB572, mand_, single_, o_EbmlMaster ) \
+   XMAP_C_( O_Cmc, "CMC", 0x11cab572, mand_, single_, o_EbmlMaster ) \
    \
-   XMAP_C_( O_CmcInfo, "Info", 0x1, mand_, single_, o_EbmlMaster ) \
+   XMAP_C_( O_CmcInfo, "Info", 0x11ab94c5, mand_, single_, o_EbmlMaster ) \
    \
    XMAP_C_( O_CmcRelease, "Release", 0x1, mand_, single_, o_EbmlMaster ) \
    XMAP_C_( O_CmcTitle, "Title", 0x1, mand_, single_, o_EbmlUtf8 ) \
@@ -27,13 +26,13 @@
    XMAP_C_( O_CmcImprint, "Imprint", 0x1, opt_, single_, o_EbmlUtf8 ) \
    XMAP_C_( O_CmcLanguage, "Language", 0x1, opt_, single_, o_EbmlString ) \
    \
-   XMAP_C_( O_CmcCreators, "Creators", 0x1, opt_, single_, o_EbmlMaster ) \
-   XMAP_C_( O_CmcCreator, "Creator", 0x1, mand_, mult_, o_EbmlMaster ) \
+   XMAP_C_( O_CmcCreators, "Creators", 0x14b658f4, opt_, single_, o_EbmlMaster ) \
+   XMAP_C_( O_CmcCreator, "Creator", 0x2c103f, mand_, mult_, o_EbmlMaster ) \
    XMAP_C_( O_CmcCreatorName, "CreatorName", 0x1, mand_, single_, o_EbmlUtf8 ) \
    XMAP_C_( O_CmcCreatorJob, "CreatorJob", 0x1, mand_, single_, o_EbmlString ) \
    XMAP_C_( O_CmcCreatorPages, "CreatorPages", 0x1, opt_, single_, o_EbmlString ) \
    \
-   XMAP_C_( O_CmcIssues, "Issues", 0x1, opt_, single_, o_EbmlMaster ) \
+   XMAP_C_( O_CmcIssues, "Issues", 0x3eef6c, opt_, single_, o_EbmlMaster ) \
    XMAP_C_( O_CmcIssue, "Issue", 0x1, mand_, mult_, o_EbmlMaster ) \
    XMAP_C_( O_CmcIssueSeries, "IssueSeries", 0x1, mand_, single_, o_EbmlUtf8 ) \
    XMAP_C_( O_CmcIssueVolume, "IssueVolume", 0x1, mand_, single_, o_EbmlUint ) \
@@ -45,12 +44,12 @@
    XMAP_C_( O_CmcIssueLanguage, "IssueLanguage", 0x1, opt_, single_, o_EbmlString ) \
    XMAP_C_( O_CmcIssuePages, "IssuePages", 0x1, opt_, single_, o_EbmlString ) \
    \
-   XMAP_C_( O_CmcStories, "Stories", 0x1, opt_, single_, o_EbmlMaster ) \
+   XMAP_C_( O_CmcStories, "Stories", 0x1bd8eb6f, opt_, single_, o_EbmlMaster ) \
    XMAP_C_( O_CmcStory, "Story", 0x1, mand_, mult_, o_EbmlMaster ) \
    XMAP_C_( O_CmcStoryTitle, "StoryTitle", 0x1, mand_, single_, o_EbmlUtf8 ) \
    XMAP_C_( O_CmcStoryPages, "StoryPages", 0x1, opt_, single_, o_EbmlString ) \
    \
-   XMAP_C_( O_CmcPages, "Pages", 0x1, mand_, single_, o_EbmlMaster ) \
+   XMAP_C_( O_CmcPages, "Pages", 0x15e30a78, mand_, single_, o_EbmlMaster ) \
    XMAP_C_( O_CmcPage, "Page", 0x1, mand_, mult_, o_EbmlMaster ) \
    XMAP_C_( O_CmcPageNumber, "PageNumber", 0x1, mand_, single_, o_EbmlUint ) \
    XMAP_C_( O_CmcPageContent, "PageContent", 0x1, opt_, mult_, o_EbmlString ) \
